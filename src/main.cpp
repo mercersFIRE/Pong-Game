47eebf597596ae4f244bd99df868fa00d3ecf510
mercersFIRE <imranurrahman16@gmail.com>
2025-02-05 19:12:02 +0600

    Updating workspace

--- src/main.cpp
+++ src/main.cpp
@@ -5,40 +5,50 @@ struct ball
     int x=400,y=400,rad=50;
     Color color=RED;
 };
+struct rectangle
+{
+    int x=400,y=0,width=250,height=20;
+    Color color=RED;
+};
+
 
 int main()
 {
-    int width = 1280, height = 720;
-    InitWindow(width, height, "test");
+    int width = 1280, height = 720,x=5,y=5;
+    InitWindow(width, height, "Ponng Game");
     InitAudioDevice();
     SetTargetFPS(70);
     std::cout<<"Hello, World!"<<std::endl;
     Sound bounce = LoadSound("res/bounce.mp3");
     ball a;
+    rectangle rup,rdown;
     
-    int x=5,y=5;
+    HideCursor();
     while (!WindowShouldClose())
     {
-        a.x+=x;
+        
+        a.x+=x;a.y+=y;
         if (a.x+a.rad>=width || a.x-a.rad<=0)
         {
             x*=-1;
             PlaySound(bounce);
             a.color = { (unsigned char)GetRandomValue(0, 255), (unsigned char)GetRandomValue(0, 255), (unsigned char)GetRandomValue(0, 255), 255 };
         }
-        a.y+=y;
         if (a.y+a.rad>=height || a.y-a.rad<=0)
         {
             y*=-1;
             PlaySound(bounce);
             a.color={ (unsigned char)GetRandomValue(0, 255), (unsigned char)GetRandomValue(0, 255), (unsigned char)GetRandomValue(0, 255), 255 };
         }
+
+        DrawRectangle(GetMouseX(), height-rdown.height, rdown.width  , rdown.height, RED);
+        std::cout<<GetMouseX()<<std::endl;
+
         ClearBackground(BLACK);
         BeginDrawing();
         DrawLineEx(Vector2{0, (float)height/2}, Vector2{1280, (float)height/2}, 5, DARKBROWN);
         DrawText("Hello, World!", 10, 10, 20, DARKGRAY);
         DrawCircleGradient(a.x, a.y, a.rad, a.color, GRAY);
-
         DrawFPS(500, 10);
         EndDrawing();
     }
