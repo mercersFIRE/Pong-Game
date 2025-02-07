9f32a6c59463d5102999119bdef1a176f42da990
mercersFIRE <imranurrahman16@gmail.com>
2025-02-07 12:44:48 +0600

    added keypress

--- src/main.cpp
+++ src/main.cpp
@@ -32,28 +32,29 @@ public:
         this->y += speedy;
     }
 
-    int lost(float rdowny, Sound &lost)
+    int lost(float rdowny, Sound &lost, long long &score)
     {
         if(this->y + this->rad > rdowny){
             int textWidth = MeasureText("Press SPACE To Play Again", 50);
             PlaySound(lost);
             float alpha = 0.0f;
-            while (!IsKeyPressed(KEY_SPACE))
+            while (!(IsKeyPressed(KEY_SPACE)||IsMouseButtonPressed(MOUSE_LEFT_BUTTON)))
             {
                 ClearBackground(BLACK);
                 BeginDrawing();
                 alpha += 0.01f;
                 if (alpha > 1.0f) alpha = 1.0f;
                 DrawText("Press SPACE To Play Again", (GetScreenWidth() - textWidth) / 2, (GetScreenHeight() - 50) / 2, 50, Fade(WHITE, alpha));
-                DrawText(TextFormat("BallY Position: %.2f rdown: %.2f ", this->y,rdowny), (GetScreenWidth() - MeasureText(TextFormat("BallY Position: %.2f rdown: %.2f ", this->y,rdowny),50) ) / 2, (GetScreenHeight() + 50) / 2, 50, Fade(WHITE, alpha));
+                DrawText(TextFormat("Your Score : %ld", score), (GetScreenWidth() - MeasureText(TextFormat("Your Score : %ld", score),50) ) / 2, (GetScreenHeight() + 50) / 2, 50, Fade(WHITE, alpha));
                 EndDrawing();
-                if(WindowShouldClose())return 1;
+                if(WindowShouldClose()||IsMouseButtonPressed(MOUSE_RIGHT_BUTTON))return 1;
             }
             this->x=GetScreenWidth()/2;
             this->y=GetScreenHeight()/2;
             this->speedx=3;
             this->speedy=3;
             this->speedtotal=6;
+            score=0;
         }
         return 0;
     }
@@ -66,22 +67,23 @@ public:
     Color color = {232, 181, 72,100};
     void draw(float ColorAlpha)
     {
-        DrawRectangleRounded(Rectangle{std::min(this->x = GetMouseX(),GetScreenWidth()- this->width), this->y = GetScreenHeight() - this->height-20, this->width, this->height},.5,0,Fade( this->color,ColorAlpha));
+        DrawRectangleRounded(Rectangle{std::min(this->x = GetMouseX(),GetScreenWidth()- this->width), this->y = GetScreenHeight() - this->height-20, this->width, this->height},.9,0,Fade( this->color,ColorAlpha));
     }
 };
 int loadmenu()
 {
     int textWidth = MeasureText("Press SPACE To Start", 50);
     float alpha = 0.0f;
-    while (!IsKeyPressed(KEY_SPACE))
+    while (!(IsKeyPressed(KEY_SPACE)||IsMouseButtonPressed(MOUSE_LEFT_BUTTON)))
     {
         ClearBackground(BLACK);
         BeginDrawing();
         alpha += 0.01f;
         if (alpha > 1.0f) alpha = 1.0f;
         DrawText("Press SPACE To Start", (GetScreenWidth() - textWidth) / 2, (GetScreenHeight() - 50) / 2, 50, Fade(WHITE, alpha));
+        DrawText("Press ESC To Exit", (GetScreenWidth() - MeasureText("Press ESC To Exit", 50)) / 2, (GetScreenHeight() + 50) / 2, 50, Fade(WHITE, alpha));
         EndDrawing();
-        if(WindowShouldClose())return 1;
+        if(WindowShouldClose()||IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))return 1;
     }
     return 0;
 }
@@ -101,9 +103,9 @@ int main()
     if(loadmenu())return 1;
 
     HideCursor();
-    while (!WindowShouldClose())
+    while (!IsKeyPressed(KEY_ESCAPE))
     {
-        if(ball.lost(rdown.y,lost))break;
+        if(ball.lost(rdown.y,lost,score))break;
         ball.move(bounce);
 
         if(CheckCollisionCircleRec(Vector2{ball.x,ball.y},ball.rad,Rectangle{rdown.x,rdown.y,rdown.width,rdown.height})){
@@ -114,7 +116,7 @@ int main()
             ball.speedtotal+=.5;
             ball.move(bounce);
             PlaySound(bounce);
-            score+=ball.speedtotal;
+            score+=ball.speedtotal/2;
             ball.alpha=0.0f;
             ball.color = {(unsigned char)GetRandomValue(0, 255), (unsigned char)GetRandomValue(0, 255), (unsigned char)GetRandomValue(0, 255), 255};
         }
@@ -122,6 +124,7 @@ int main()
         ColorAlpha += 0.01f;
         ClearBackground(BLACK);
         BeginDrawing();
+        DrawText(TextFormat("Score: %lld", score), 10, 50, 50, Fade(WHITE, ColorAlpha));
         DrawLineEx(Vector2{0, (float)height / 2}, Vector2{(float)GetScreenWidth(), (float)height / 2}, 5, Fade(DARKBROWN,ColorAlpha));
         rdown.draw(ColorAlpha);
         ball.draw(ColorAlpha);
@@ -129,7 +132,10 @@ int main()
         EndDrawing();
         if(!IsSoundPlaying(music))PlaySound(music);
     }
+    if(loadmenu())return 1;
     UnloadSound(bounce);
+    UnloadSound(lost);
+    UnloadSound(music);
     CloseAudioDevice();
     CloseWindow();
     return 0;
